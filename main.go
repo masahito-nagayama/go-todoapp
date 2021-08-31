d6c75be1e95b55a6c8708166e146054ad589beb8
masahito-nagayama <me18sf67m@gmail.com>
2021-08-31 16:41:15 +0900

    fix base.go

--- main.go
+++ main.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"go_todoapp/config"
+	"go_todoapp/app/models"
 )
 
 func main() {
-	fmt.Println(config.Config.Port)
-	fmt.Println(config.Config.SQLDriver)
-	fmt.Println(config.Config.DbName)
-	fmt.Println(config.Config.LogFile)
+	// fmt.Println(config.Config.Port)
+	// fmt.Println(config.Config.SQLDriver)
+	// fmt.Println(config.Config.DbName)
+	// fmt.Println(config.Config.LogFile)
 
-	log.Println("test")
+	// log.Println("test")
+	fmt.Println(models.Db)
 }
