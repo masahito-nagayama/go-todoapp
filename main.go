9c209b54e808c6c116f23dfe98fb154495397c90
masahito-nagayama <me18sf67m@gmail.com>
2021-09-02 08:08:10 +0900

    add getuser

--- main.go
+++ main.go
@@ -14,12 +14,15 @@ func main() {
 	// log.Println("test")
 	fmt.Println(models.Db)
 
-	u := &models.User{}
-	u.Name = "test"
-	u.Email = "test@example.com"
-	u.PassWord = "testtest"
+	// u := &models.User{}
+	// u.Name = "test"
+	// u.Email = "test@example.com"
+	// u.PassWord = "testtest"
+	// fmt.Println(u)
+
+	// u.CreateUser()
+
+	u, _ := models.GetUser(1)
+
 	fmt.Println(u)
-
-	u.CreateUser()
-
 }
