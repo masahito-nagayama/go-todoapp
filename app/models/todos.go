5f6ac315446bee583163f9b33e74af2b508f4596
masahito-nagayama <me18sf67m@gmail.com>
2021-09-02 09:25:06 +0900

    createTODO

---
+++ app/models/todos.go
@@ -0,0 +1,26 @@
+package models
+
+import (
+	"time"
+	"log"
+)
+
+type Todo struct {
+	ID int
+	Content string
+	UserID int
+	CreatedAt time.Time
+}
+
+func (u *User) CreateTodo(content string) (err error) {
+	cmd := `INSERT into todos (
+		content, 
+		user_id, 
+		created_at) values (?, ?, ?)`
+
+	_, err = Db.Exec(cmd, content, u.ID, time.Now())
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
+}
