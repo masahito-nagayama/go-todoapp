41e885b5785900f3a75f82b0fb7901de7f295f58
masahito-nagayama <me18sf67m@gmail.com>
2021-09-06 08:35:38 +0900

    add gettodosbyuser

--- main.go
+++ main.go
@@ -6,11 +6,11 @@ import (
 )
 
 func main() {
-	fmt.Println(models.Db)
+	// fmt.Println(models.Db)
 
 	// u := &models.User{}
-	// u.Name = "test"
-	// u.Email = "test@example.com"
+	// u.Name = "test2"
+	// u.Email = "test2@example.com"
 	// u.PassWord = "testtest"
 	// fmt.Println(u)
 
@@ -30,14 +30,19 @@ func main() {
 	// u, _ = models.GetUser(1)
 	// fmt.Println(u)
 
-	user, _ := models.GetUser(2)
-	user.CreateTodo("Second Todo") 
+	// user, _ := models.GetUser(3)
+	// user.CreateTodo("Third Todo") 
 
 	// t, _ := models.GetTodo(1)
 	// fmt.Println(t)
 
-	todos, _ := models.GetTodos()
+	// todos, _ := models.GetTodos()
+	// for _, v := range todos {
+	// 	fmt.Println(v)
+	// }
+	user2, _ := models.GetUser(3)
+	todos, _ := user2.GetTodosByUser()
 	for _, v := range todos {
 		fmt.Println(v)
-	}
+	} 
 }
