ac6829f63a26c04dec2f4b4c9f2ba8e9b8b20a24
masahito-nagayama <me18sf67m@gmail.com>
2021-09-23 09:01:57 +0900

    add GetUserByEmail func

--- app/models/users.go
+++ app/models/users.go
@@ -14,6 +14,14 @@ type User struct {
 	CreatedAt time.Time
 }
 
+type Session struct {
+	ID int
+	UUID string
+	Email string
+	UserID int
+	CreatedAt time.Time 
+}
+
 func (u *User) CreateUser() (err error) {
 	cmd := `insert into users (
 		uuid,
@@ -67,4 +75,11 @@ func (u *User) DeleteUser() (err error) {
 		log.Fatalln(err)
 	}
 	return err
+}
+
+func GetUserByEmail(email string) (user User, err  error) {
+	user = User{}
+	cmd := `select id, uuid, name, email, password, created_at
+	form users where id = ?`
+
 }
