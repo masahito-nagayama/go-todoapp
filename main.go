c9ec54b05b60fdf637c82bc3a69c8675f176525b
masahito-nagayama <me18sf67m@gmail.com>
2021-09-24 17:05:37 +0900

    add sesssion

--- main.go
+++ main.go
@@ -2,21 +2,13 @@ package main
 
 import (
 	"fmt"
-	"log" 
-	"go_todoapp/app/controllers"
+	// "log" 
 	"go_todoapp/app/models"
+	"go_todoapp/app/controllers"
 )
 
 func main() {
 	fmt.Println(models.Db)
 
-	// controllers.StartMainServer()
-	user, _ := models.GetUserByEmail("test@example.com")
-	fmt.Println(user)
-
-	session, err := user.CreateSession()
-	if err != nil {
-		log.Println(err)
-	}
-	fmt.Println(session)
+	controllers.StartMainServer()
 }
